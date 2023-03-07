349c777c2165ca9e3c3d8709bcfb8e7970dd80b8
Florian Gubler <gubler.florian@gmx.net>
2023-03-07 14:03:11 +0100

    Tried unit test with context mock

--- app/src/test/java/io/github/floriangubler/quaky/ExampleUnitTest.java
+++ app/src/test/java/io/github/floriangubler/quaky/ExampleUnitTest.java
@@ -1,8 +1,17 @@
 package io.github.floriangubler.quaky;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import android.content.Context;
+
+import java.io.IOException;
+
+import io.github.floriangubler.quaky.entity.Earthquake;
+import io.github.floriangubler.quaky.service.LastEarthquakeService;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +19,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    Context context;
+    @Before
+    public void start(){
+        context = mock(Context.class);
+    }
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void addition_isCorrect() throws IOException {
+        //Doesnt work must mock context for service
+        LastEarthquakeService fileService = new LastEarthquakeService();
+        fileService.setLastEarthquake(new Earthquake());
+        assertEquals(fileService.getLastEarthquake(), new Earthquake());
     }
 }
